f5bd84db6aa1a95c3c7d32b9a8a9f90a260d72bb
yaasin-raki2 <yassin23raki@gmail.com>
2020-12-15 21:19:28 +0100

    added Echo3

---
+++ 4-Echo3/echo3.go
@@ -0,0 +1,13 @@
+// Another implementation of the echo command using the Join method from the strings package
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	fmt.Println(strings.Join(os.Args[1:], " "))
+}
