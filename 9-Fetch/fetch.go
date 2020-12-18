4ece0dee95e623f17b741cafeb033c1115774711
yaasin-raki2 <yassin23raki@gmail.com>
2020-12-18 11:20:11 +0100

    added Fetch.

---
+++ 9-Fetch/fetch.go
@@ -0,0 +1,33 @@
+// Fetch prints the content found at a URL.
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	var prefix string = "https://"
+	for _, url := range os.Args[1:] {
+		if strings.HasPrefix(url, prefix) == false {
+			url = prefix + url
+		}
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Printf("Status Code : %s\n", resp.Status)
+		data, err := io.Copy(os.Stdout, resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		fmt.Println(data)
+	}
+}
